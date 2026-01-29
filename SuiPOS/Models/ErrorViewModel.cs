1823120e3d39aa5b4d269799bebf5c2d5ceed958
trungnghia-coder <nguyentrunghia0907@gmail.com>
2026-01-29 17:09:24 +0700

    refactor: rebuild MVC architecture core

---
+++ SuiPOS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SuiPOS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
